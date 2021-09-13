c15595d0987aee5be49c32997866497ee7404110
beginor <beginor@qq.com>
2021-09-13 06:03:00 +0000

    cleanup

--- src/app/pages/todo.tsx
+++ src/app/pages/todo.tsx
@@ -1,39 +1,7 @@
-import { useEffect, useState } from 'react';
-
-import { loadData } from '../common/load-data';
-
 import withLoading from '../controls/with-loading';
 
 function Todo(props: TodoProps): JSX.Element {
-    // const [data, setData] = useState<TodoItem[]>([]);
-    // const [isFetching, setIsFetching] = useState(false);
-
-    // const loadData = async function(url: string): Promise<void> {
-    //     setIsFetching(true);
-    //     const res = await fetch(url);
-    //     const data = await res.json();
-    //     setData(data);
-    //     setIsFetching(false);
-    // }
-
-    // useEffect(() => {
-    //     // loadData(props.url);
-    //     setIsFetching(true);
-    //     void loadData<TodoItem[]>(props.url)
-    //         .then(data => setData(data))
-    //         .finally(() => setIsFetching(false));
-    // },[]);
-
     return (
-    //   <>
-    //   {
-    //     isFetching
-    //       ? (<span>Loading todo items ...</span>)
-    //       : (<ul>
-    //           { data.map(item => (<li key={ item.id }>{ item.title }</li>)) }
-    //         </ul>)
-    //   }
-    //   </>
       <ul>
         { props.data?.map(item => (<li key={ item.id }>{ item.title }</li>)) }
       </ul>
@@ -48,6 +16,5 @@ export interface TodoItem {
 }
 
 export interface TodoProps {
-    url?: string;
     data?: TodoItem[];
 }
