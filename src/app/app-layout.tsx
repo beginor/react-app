957657379e9eac2f2989bd8ceab241176a8526f3
beginor <beginor@qq.com>
2021-12-23 20:51:25 +0800

    rename

--- src/app/app-layout.tsx
+++ src/app/app-layout.tsx
@@ -1,7 +1,7 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 
-import Nav from './controls/nav';
+import Nav from './controls/nav-bar';
 import './app-layout.scss';
 
 export default function AppLayout(props: AppLayoutProps): JSX.Element {
