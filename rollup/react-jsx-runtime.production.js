299dd5025b33948684b2ad66167adba38cb5f27f
beginor <beginor@qq.com>
2022-01-10 10:16:21 +0800

    cleanup

--- rollup/react-jsx-runtime.production.js
+++
@@ -1,6 +0,0 @@
-import JSX from 'react/cjs/react-jsx-runtime.production.min.js';
-export default JSX;
-export const { jsx, jsxs, Fragment } = JSX;
-
-export const __esModule = true;
-export const __esmModule = true;
